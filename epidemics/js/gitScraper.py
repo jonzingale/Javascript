6528bb197d3d3240fd4d0b863cd1cc8c71aa8117
Jon Zingale <jonzingale@gmail.com>
2019-04-25 12:36:57 -0600

    better graph scraping

--- epidemics/js/gitScraper.py
+++ epidemics/js/gitScraper.py
@@ -12,19 +12,21 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class Agent:
   def __init__(self):
     self.http = urllib3.PoolManager()
-    self.graph = Graph()
+    self.file = open("gitData.json", "w")
     self.json = json.JSONEncoder()
-    self.file = open("gitData.txt", "a")
+    self.graph = Graph()
 
   def get_neighbors(self, user_stub):
-    html = self.http.request('GET', URL_FORMAT % user_stub).data
-    page = BeautifulSoup(html, features="lxml")
+    html = self.http.request('GET', URL_FORMAT % user_stub)
+    page = BeautifulSoup(html.data, features="lxml")
     ns = page.body.find_all('a', attrs={'class':LINK_CLASS})  
     return([href.get('href') for href in ns])
 
   def walk_network(self, node, radius):
     if (radius > 0):
       ns = self.get_neighbors(node)
+      # limit leaves for a node
+      if (radius <= 1): ns = ns[0:20]
       self.graph.add_subgraph(node, ns)
       for neighbor in ns:
         self.walk_network(neighbor, radius-1)
@@ -34,18 +36,14 @@ class Agent:
 
 class Graph:
   def __init__(self):
-    self.json = {}
-    self.nodes = self.json.keys()
     self.edges = self.json.values()
+    self.nodes = self.json.keys()
+    self.json = {}
 
   # subgraph :: {user_stub: [user_stubs]}
   def add_subgraph(self, node, edges):
     self.json[node] = edges
 
 agent = Agent()
-agent.walk_network(CENTER_NODE, 3)
+agent.walk_network(CENTER_NODE, 2)
 agent.data_writer()
-
-# TODO: restrict links to degree 5 and go deeper
-
-
