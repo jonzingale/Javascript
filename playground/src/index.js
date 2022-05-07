c743b47e78f868f19b2e5f894afddd1b44bea605
Jon Zingale <jonzingale@gmail.com>
2022-05-07 13:45:21 -0600

    fixups

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,16 +1,17 @@
 import { ls } from './linear.js';
 import '/src/render.js';
 import '/src/hypercube.js';
-import * as math from 'mathjs';
+import { multiply } from 'mathjs';
 
 // returns the basis factorization for the solution vector.
 function component(vect = [1,1,1,1,1]) {
   const element = document.createElement('div');
   element.innerHTML =
-    math.multiply(ls, vect)
+    multiply(ls, vect)
       .map(x => x % 2)
       ._data.join(' ');
   return element;
 }
 
 document.body.appendChild(component([0,1,1,0,1]));
+document.body.appendChild(component([0,1,1,1,0]));
