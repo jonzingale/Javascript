9a6f3a487ecb7af2f0a064d5766b5d1c93d02e4f
Jon Zingale <jonzingale@gmail.com>
2022-05-10 17:32:50 -0600

    added colors to constants

--- playground/src/constants.js
+++ playground/src/constants.js
@@ -2,4 +2,17 @@ const width = 400;
 const height = 400;
 const size = 100;
 
-export { width, height, size };
+const colors = [
+'#309471', // Spruce Green
+'#0f0f0f', // Black
+'#483c32', // Taupe
+'#b69b80', // Light Umber 80%
+'#fcdec4', // Brick Beige
+'#c57b62', // Nectar
+'#d1bfac', // Light Umber 40%
+'#55565d', // Neutral Grey 80%
+'#f0b58b', // Peach
+'#ab4e46', // Henna
+]
+
+export { width, height, size, colors };
